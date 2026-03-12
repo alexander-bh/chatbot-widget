3581958f75f44c62cc5694fcd27983e89fbe0ec8
alexander-bh <alexanderbh.tonali@gmail.com>
2026-03-12 16:48:55 -0600

    Actualizar el frond

--- src/chatbot/useChatbot.ts
+++ src/chatbot/useChatbot.ts
@@ -483,7 +483,7 @@ export function useChatbot(config: ChatbotConfig | null) {
                     appendMessage("user", o.label)
 
                     // enviar al backend
-                    await sendFn(o.value ?? o.label)
+                    await sendFn(o.value || o.label)
                 }
 
                 optionsContainer.appendChild(btn)
